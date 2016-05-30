4566e611049f5ceda7bc40fb3b558f6fd73a44d8
Andrew Thomas <andr3w.l.thomas@gmail.com>
2016-05-30 01:49:58 -0500

    updated models, and reset migrations, editable = false makes testing hard :)

--- wall/models.py
+++ wall/models.py
@@ -6,9 +6,9 @@ from django.contrib.auth.models import User
 
 class Post(TimeStampedModel):
     content = models.TextField()
-    user = models.ForeignKey(User, editable=False, related_name="posts")
+    user = models.ForeignKey(User, related_name="posts")
     emotion = models.ForeignKey('Emotion', related_name="posts")
-    parent = models.ForeignKey('Post', editable=False, blank=True, null=True,
+    parent = models.ForeignKey('Post', blank=True, null=True,
                                related_name='replies')
 
 
@@ -18,7 +18,7 @@ class Emotion(models.Model):
 
 class Like(TimeStampedModel):
     post = models.ForeignKey('Post', related_name='likes')
-    user = models.ForeignKey(User, editable=False, related_name="likes")
+    user = models.ForeignKey(User, related_name="likes")
 
 
 class Notification(TimeStampedModel):
@@ -33,6 +33,4 @@ class Notification(TimeStampedModel):
     content = models.TextField()
     method = models.CharField(max_length=50, choices=METHODS)
     trigger = models.CharField(max_length=50, choices=TRIGGERS)
-    user = models.ForeignKey(User, editable=False,
-                             related_name="notifications")
-
+    user = models.ForeignKey(User, related_name="notifications")
