4151784704da3d74f5c24962bb97037c710df734
Andrew Thomas <andr3w.l.thomas@gmail.com>
2016-06-04 02:46:45 -0500

    added user serializer and updated post serializer to send an ‘id’ field with the pk as well as a user object instead of just the pk

--- wall/serializers.py
+++ wall/serializers.py
@@ -1,12 +1,23 @@
 from rest_framework import serializers
 
+from django.contrib.auth.models import User
+
 from wall.models import Post, Emotion, Like, Notification
 
 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('pk', 'first_name', 'last_name', 'email', 'username')
+
+
 class PostSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+    id = serializers.ReadOnlyField(source='pk')
+
     class Meta:
         model = Post
-        fields = ('content', 'user', 'emotion', 'parent')
+        fields = ('id', 'content', 'user', 'emotion', 'parent')
 
 
 class EmotionSerializer(serializers.ModelSerializer):
@@ -25,3 +36,6 @@ class NotificationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Notification
         fields = ('content', 'method', 'trigger', 'user')
+
+
+
